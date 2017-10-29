1c83da20fbdd8a9a25e554ef8e2fe7301cbb5e9b
vhasselmann <vhasselmann@gmail.com>
2017-10-29 14:12:25 +0000

    Game Input

---
+++ Beautiful Monsters/Assets/Scripts/GameInput/IInteractable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IInteractable
+{
+	void OnPointerDown (MyTouch touch);
+	void OnPointerDrag (MyTouch touch);
+	void OnPointerUp (MyTouch touch);
+	void OnPointerUpAsButton (MyTouch touch);
+}
